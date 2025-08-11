15dff6c687bb2e7ae9598840669d0e0b681e6d77
Suruchi <161755548+suruchi810@users.noreply.github.com>
2025-08-11 13:44:53 +0530

    Update admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Product, CartItem, WishlistItem, Order, OrderItem
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(CartItem)
+admin.site.register(WishlistItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+
